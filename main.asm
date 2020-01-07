4d885026096cd7615e77f2bba27631c7f0646c4e
Andy C <a9c@dr.com>
2020-01-07 19:57:13 +0000

    Debugging update
    I had a cheap chinese ATTINY13A clone, which I think I mistakenly reduced the clock speed to /64 and the timer to /1024. Once this was done, I could no longer program it. This update is flashed onto a genuine ATTINY13A with no probs.

--- main.asm
+++ main.asm
@@ -1,7 +1,7 @@
 ;
 ; attiny13A-Bikelock.asm
 ;
-; ATTINY13A Xplained board via ISP
+; ATTINY13A board via ISP
 .nolist
 .include "tn13adef.inc" ; Define device ATtiny13A
 .list
@@ -11,14 +11,14 @@
 ; **********************************
 ;
 ; Device: ATtiny13A, Package: 8-pin-PDIP_SOIC
-;
-;					  _________
-;				       1 /         |8
+;				
+;						   		      _________
+;								   1 /         |8
 ;	(PCINT5/RESET/ADC0/dW) PB5   o--|RESET  VCC|--o  VCC
 ;	(PCINT3/CLKI/ADC3) PB3       o--|PB3    PB2|--o  PB2 (SCK/ADC1/T0/PCINT2)
-;	(PCINT4/ADC2) PB4	     x--|PB4    PB1|--x  PB1 (MISO/AIN1/OC0B/INT0/PCINT1)
-;			       GND   o--|GND    PB0|--o  PB0 (MOSI/AIN0/OC0A/PCINT0)
-;				      4 |__________|5 
+;	(PCINT4/ADC2) PB4			 x--|PB4    PB1|--x  PB1 (MISO/AIN1/OC0B/INT0/PCINT1)
+;						   GND   o--|GND    PB0|--o  PB0 (MOSI/AIN0/OC0A/PCINT0)
+;								  4 |__________|5 
 ; Push button input attached to PORTB, PIN1
 ; LED +ve side attached to PORTB, PIN4
 ; ISP programming connections on MOSI,MISO,SCK,VCC,GND AND RESET
@@ -52,7 +52,7 @@
 	reti			;TIM0_COMPA
 	reti			;TIM0_COMPB
 	reti			;WDT
-	reti			;ADC
+	reti			;ADC	
 
 
 ; **********************************
@@ -78,9 +78,9 @@ reti
 lighton:
 	push rmp
 	Sbi PORTB,PORTB4		;LED on
-	rcall delay10sec		;10 Second delay
-	rcall delay10sec		;10 Second delay
-	rcall delay10sec		;10 Second delay
+	rcall delay10sec		;8 Second delay
+	rcall delay10sec		;8 Second delay
+	rcall delay10sec		;8 Second delay
 	cbi PORTB,PORTB4		;LED off
 	pop rmp
 	ret
@@ -90,13 +90,13 @@ delay10sec:
 	push r19
 	ldi r19,150			;initialise r19 for a 10 second timer delay
 ;	ldi r19,15			;1 second delay for debugging
-	ldi rmp,(1<<CS02)|(1<<CS00)
+	ldi rmp,(1<<CS02)|(0<<CS01)|(0<<CS00)	;Start the timer, no prescaler
 	sei					;Enable interrupts (for the 16 bit timer overflow)
-	OUT TCCR0B,rmp		;start the 16 bit timer
+	OUT TCCR0B,rmp		;start the timer
 back:	cpi r19,0
 	brne back
 	cli					;Disable interrupts
-	ldi rmp,(0<<CS02)|(0<<CS00)
+	ldi rmp,(0<<CS02)|(0<<CS01)|(0<<CS00)	;Stop the timer, no prescaler
 	OUT TCCR0B,rmp		;stop the timer
 	pop r19
 	pop rmp				;restore the rmp register
@@ -105,12 +105,12 @@ back:	cpi r19,0
 lightonshort:
 	push rmp
 	ldi rmp,0x04			;Counter for number of flashes
-top:	Sbi PORTB,PORTB4		;LED on
+top:	Sbi PORTB,PORTB4	;LED on
 	rcall delayshort		;Delay
 	cbi PORTB,PORTB4		;LED off
 	rcall delayshort		;Delay
 	dec rmp
-	brne top			;Loop for flashes
+	brne top				;Loop for flashes
 	pop rmp
 	ret
 ;--------------
@@ -118,12 +118,12 @@ delayshort:
 	push rmp			;save the rmp register	
 	push r19
 	ldi r19,3			;initialise r19
-	ldi rmp,(1<<CS02)|(1<<CS00)
-	OUT TCCR0B,rmp			;start the 8 bit timer
+	ldi rmp,(1<<CS02)|(0<<CS01)|(0<<CS00)	;Start the timer, no prescaler
+	OUT TCCR0B,rmp		;start the timer
 backshort:	cpi r19,0
 	brne backshort
-	ldi rmp,(0<<CS02)|(0<<CS00)
-	OUT TCCR0B,rmp			;stop the timer
+	ldi rmp,(0<<CS02)|(0<<CS01)|(0<<CS00)	;Stop the timer, no prescaler
+	OUT TCCR0B,rmp		;stop the timer
 	pop r19
 	pop rmp				;restore the rmp register
 	ret
@@ -134,22 +134,22 @@ backshort:	cpi r19,0
 
 Main:
 ;-------------STACK POINTER SETUP----------
-.ifdef SPH			; if SPH is defined
+.ifdef SPH				; if SPH is defined
   ldi rmp,High(RAMEND)
   out SPH,rmp			; Init MSB stack pointer
   .endif
 	ldi rmp,Low(RAMEND)
-	out SPL,rmp		; Init LSB stack pointer
+	out SPL,rmp			; Init LSB stack pointer
 ;-------------CLOCK PRESCALER SETUP----------
-	cli
-	ldi rmp,0b10000000	; Load 1 to CLKPCE (prescaler change enable)
-	ldi r18,0b00000110	; Load 1 to CLKPS2 and CLKPS1 FOR CLOCK/64 SPEED
-	out CLKPR,rmp		
-	OUT CLKPR,r18		
-
+;	cli
+;	ldi rmp,0b10000000	; Load 1 to CLKPCE (prescaler change enable)
+;	ldi r18,0b00000110	; Load 1 to CLKPS2 and CLKPS1 FOR CLOCK/64 SPEED
+;	out CLKPR,rmp		
+;	OUT CLKPR,r18		
+nop
 ;-------------SETUP - INPUT / OUTPUT SIGNALS----------
 	sbi DDRB,DDB4		; LED OUTPUT ON PA4
-	cBI DDRB,DDB1		; Push button input on PB1
+	SBI DDRB,DDB1		; Push button input on PB1
 	sbi PORTB,PORTB1	; Pullup enable on pB1 button
 	CBI PORTB,PORTB4	; LED OFF
 
@@ -159,11 +159,11 @@ Main:
 
 ;-------------ENABLE INTERRUPTS----------
 
-	ldi rmp,(1<<ISC01)	; INT0 interrupt - falling edge
+	ldi rmp,1<<ISC01	; INT0 interrupt - falling edge
 	out MCUCR,rmp		; MCU CONTROL REGISTER
 	ldi rmp,1<<INT0		; Enable INT0
 	out GIMSK,rmp		; Interrupt mask register
-	sei			; Enable global interrupts
+	sei					; Enable global interrupts
 
 ;-------------SLEEP MODE ENABLE----------
 	ldi rmp,(1<<SM1)|(1<<SE)  ; power down sleep mode | sleep enable 
